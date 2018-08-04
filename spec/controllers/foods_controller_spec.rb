e3992c3904279ca43b900be5aa4b1fd5b9735b6c
Luthfian Hakim <luthfian.hakim@go-jek.com>
2018-08-04 23:43:14 +0700

    add spec for new action for food controller

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -46,8 +46,15 @@ describe FoodsController do
   end
 
   describe 'GET #new' do
-    it "assigns a new Food to @food"
-    it "renders the :new template"
+    it "assigns a new Food to @food" do
+      get :new
+      expect(assigns(:food)).to be_a_new(Food)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(:response).to render_template :new
+    end
   end
 
   describe 'GET #edit' do
